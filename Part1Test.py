deb8390eb789e3b220842dd6870828739af4d0af
isaac-gish <gish.isaac@gmail.com>
2023-06-28 16:26:30 -0500

    Updated for half of part 1

--- Part1Test.py
+++ Part1Test.py
@@ -2,8 +2,14 @@
 import os
 import csv
 
+
+
 #Pull in CSV files and path it
 csvpath = os.path.join('..', 'python-challenge-isaac-gish', 'Resources', 'budget_data.csv')
+
+#Save lists
+Months = []
+Cash = []
 
 
 #Open CSV file and read
@@ -14,14 +20,28 @@ with open(csvpath) as csvfile:
 #Read headers when reading
     csvheader = next(csvreader)
 
+    for row in (csvreader):
+        #Apply column 0 to months
+        Months.append(row[0])
+        #Apply column 1 to cash and convert cash to digits
+        Cash.append(float(row[1]))
 
-
-
-
-#Define Variables
-
-#Print Title
+#New Variables
+Total_Months = len(Months)
+Total_Cash = sum(Cash)
+#Create Title
 print("                                  Financial Analysis")
 #Make it easier to read
 print("---------------------------------------------------------------------------------------------------")
+
+print(f"Total Months: {Total_Months}")
+
+print(f"Total:  {Total_Cash}")
+
+#Define Variables 
+
+
+
+
+
 
